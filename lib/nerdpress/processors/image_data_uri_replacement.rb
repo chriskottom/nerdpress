8da4a0058feb5eaa9bab4ab88f5af1e0c37b089f
chriskottom <chris@chriskottom.com>
2017-08-21 10:13:05 +0200

    Retain Image sources that are not matched in the project

--- lib/nerdpress/processors/image_data_uri_replacement.rb
+++ lib/nerdpress/processors/image_data_uri_replacement.rb
@@ -3,13 +3,19 @@ require 'nokogiri'
 
 class NerdPress::Processors::ImageDataURIReplacement
   include NerdPress::Processor
+  include NerdPress::Logging
 
   def process
     doc = Nokogiri::HTML.fragment(input)
     doc.css('img').each do |img_tag|
       src = img_tag[:src]
-      image = NerdPress::Image.lookup(src)
-      img_tag[:src] = image.to_data_uri
+      begin
+        image = NerdPress::Image.lookup(src)
+        img_tag[:src] = image.to_data_uri
+      rescue ArgumentError => error
+        warn "No local image source found: #{ src }"
+        warn 'Retaining original souce'
+      end
     end
     doc.to_html
   end
--- test/nerdpress/processors/image_data_uri_replacement_test.rb
+++ test/nerdpress/processors/image_data_uri_replacement_test.rb
@@ -2,15 +2,23 @@ require 'test_helper'
 
 describe NerdPress::Processors::ImageDataURIReplacement do
   let(:image_path) { 'images/blue-square.png' }
-  let(:input_html) { "<img src='#{ image_path }'/>" }
+  let(:image_url) { 'http://google.com/logo.png' }
+  let(:local_source_html) { "<img src='#{ image_path }'>" }
+  let(:remote_source_html) { "<img src=\"#{ image_url }\">" }
   let(:processor) { NerdPress::Processors::ImageDataURIReplacement }
 
   before do
     NerdPress::Image.setup_import_path image_import_path
   end
 
-  it 'replaces path-based image sources with data URIs' do
+  it 'replaces local image sources with data URIs' do
     assert_match %r{<img src="data:image/png;base64,.*},
-                 processor.process(input_html)
+                 processor.process(local_source_html)
+  end
+
+  it 'leaves sources not found locally as they are' do
+    out, err = capture_subprocess_io do
+      assert_equal remote_source_html, processor.process(remote_source_html)
+    end
   end
 end
